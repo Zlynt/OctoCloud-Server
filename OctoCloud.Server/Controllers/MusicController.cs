31c2589870617d2885baa6e90d5b22c6b200649c
Zlynt <ivan.teixeira@zlyntlab.com>
2024-11-16 17:54:13 +0000

    Started implementing Music

---
+++ OctoCloud.Server/Controllers/MusicController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OctoCloud.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MusicController : ControllerBase
+    {
+    }
+}
---
+++ OctoCloud.Server/Models/Music.cs
@@ -0,0 +1,11 @@
+﻿namespace OctoCloud.Server.Models
+{
+    public class Music
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Album { get; set; }
+        public string[] Artists { get; set; }
+        public string FilePath { get; set; }
+    }
+}
