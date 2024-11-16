baab82e326ea492837ad741ecbc286534309dd0a
Zlynt <ivan.teixeira@zlyntlab.com>
2024-11-16 22:23:01 +0000

    Direct Play

--- OctoCloud.Server/Controllers/MusicController.cs
+++ OctoCloud.Server/Controllers/MusicController.cs
@@ -2,6 +2,7 @@
 using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Mvc;
 using OctoCloud.Server.Models;
+using System.IO;
 
 namespace OctoCloud.Server.Controllers
 {
@@ -11,6 +12,12 @@ namespace OctoCloud.Server.Controllers
     {
         private static string fileStorageLocation = Environment.GetEnvironmentVariable("MUSIC_FOLDER") ?? "./music";
 
+        public MusicController() : base() {
+            if (!Directory.Exists(fileStorageLocation)) Directory.CreateDirectory(fileStorageLocation);
+
+            Console.WriteLine($"Music Folder Location: {fileStorageLocation}");
+        }
+
         private static readonly IEnumerable<MusicModel> musicList = new[]
         {
             new MusicModel{
@@ -19,45 +26,44 @@ namespace OctoCloud.Server.Controllers
                 Artists=["THEFATRAT", "ANJULIE"],
                 Album="Fly Away",
                 AlbumImageURL="https://static.wixstatic.com/media/1f923f_96bfedf75d25413baba7427d16ac0692~mv2.jpg/v1/fill/w_1920,h_1080,al_c,q_90/TheFatRat%20-%20Fly%20Away.jpg",
-                FilePath="https://static.wixstatic.com/mp3/80cc5d_40909e0b38bd44c49576164e34d29e85.mp3?dn=FLY%20AWAY%20INUKSHUK%20REMIX.mp3"
+                StreamUrl="https://static.wixstatic.com/mp3/80cc5d_40909e0b38bd44c49576164e34d29e85.mp3?dn=FLY%20AWAY%20INUKSHUK%20REMIX.mp3"
             }
         };
 
         [HttpGet("")]
-        public MusicModel[] Get() => musicList.ToArray();
+        public MusicModel[] Get() {
+            //musicList.ToArray();
+            List<MusicModel> musics = new List<MusicModel>();
+            int counter = 0;
+            foreach (string localFilePath in Directory.GetFiles(Path.GetFullPath(fileStorageLocation), "*", SearchOption.AllDirectories))
+            {
+                string remoteFilePath = localFilePath.Replace(Path.GetFullPath(fileStorageLocation), "/Music/files").Replace("\\", "/");
+                Console.WriteLine(localFilePath);
+                musics.Add(new MusicModel
+                {
+                    Id = counter+"",
+                    Title = Path.GetFileNameWithoutExtension(remoteFilePath),
+                    //Artists = [],
+                    //Album = "",
+                    //AlbumImageURL = "",
+                    StreamUrl = remoteFilePath
+                });
+                counter++;
+            }
+            return musics.ToArray();
+        }
 
         [HttpGet("files/{*fileName}")]
         public async Task<IActionResult> Download(string fileName)
         {
-            try
-            {
-                fileName = WebUtility.UrlDecode(fileName);
-                var filePath = Path.Combine(fileStorageLocation, "files", fileName);
-                if (!System.IO.File.Exists(filePath)) { return NotFound(); }
+            var filePath = Path.GetFullPath(Path.Combine(fileStorageLocation, fileName));
 
-                var memory = new MemoryStream();
-                using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-                {
-                    await stream.CopyToAsync(memory);
-                }
-                memory.Position = 0;
-                var fileType = "application/octet-stream";
-                var result = new FileStreamResult(memory, fileType)
-                {
-                    FileDownloadName = Path.GetFileName(filePath)
-                };
-                Response.Headers.Add("Content-Disposition", new ContentDispositionHeaderValue("attachment")
-                {
-                    FileName = Path.GetFileName(filePath)
-                }.ToString());
-
-                return result;
-            }
-            catch (Exception e)
+            if (!System.IO.File.Exists(filePath))
             {
-                Console.WriteLine($"Error while downloading file {fileName}: {e.Message}");
-                return StatusCode(500, "Internal server error");
+                return NotFound();
             }
+
+            return PhysicalFile(filePath, "application/octet-stream", Path.GetFileName(filePath));
         }
     }
 }
